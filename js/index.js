5fe3ef0c3a1d6fadf716f1406ea31a953b8ff2cd
Martin Najjar <martin_najjar@yahoo.com>
2021-01-05 23:21:10 +0300

    Create a card via JavaScript

--- js/index.js
+++ js/index.js
@@ -1,13 +1,42 @@
-let myLibrary = [];
+const myLibrary = [];
+const btn = document.querySelector('#createBtn');
+const row = document.querySelector('.row');
+
+function createCard(book) {
+  const column = document.createElement('div');
+  column.classList.add('col-6');
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  const cardTitle = document.createElement('h5');
+  cardTitle.classList.add('card-title');
+  cardTitle.textContent = book.title;
+
+  const subTitle = document.createElement('h6');
+  subTitle.className = 'card-subtitle mb-2 text-muted'
+  subTitle.textContent = book.author;
+
+  const cardPages = document.createElement('p');
+  cardPages.classList.add('card-text');
+  cardPages.textContent = book.pages;
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(subTitle);
+  cardBody.appendChild(cardPages);
+  card.appendChild(cardBody);
+  column.appendChild(card);
+  row.appendChild(column);
+}
 
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
-
-let btn = document.querySelector('#createBtn');
-btn.addEventListener("click", addBookToLibrary);
 
 function addBookToLibrary() {
   let title = document.querySelector('#title');
@@ -16,5 +45,7 @@ function addBookToLibrary() {
 
   let book = new Book(title.value, author.value, pages.value);
   myLibrary.push(book);
-
+  createCard(book);
 }
+
+btn.addEventListener("click", addBookToLibrary);
